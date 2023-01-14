01c38fdcd2283e8d03048d5b477558a9241be33b
nabil75 <nabil.belcaid@free.fr>
2023-01-14 17:18:46 +0100

    20230114_black_formatter

--- all/get_all_books.py
+++ all/get_all_books.py
@@ -5,69 +5,88 @@ import csv
 import os
 import datetime
 
-#Récupérer la position d'une sous-chaîne dans une chaîne de caractères
+# Récupérer la position d'une sous-chaîne dans une chaîne de caractères
 def get_substring_position(s, str, n):
     sep = s.split(str, n)
     if len(sep) <= n:
         return -1
     return len(s) - len(sep[-1]) - len(str)
 
-#Récupérer les éléments de description d'un livre
+
+# Récupérer les éléments de description d'un livre
 def get_book_elements(url):
-    descripteurs =[]
+    descripteurs = []
     descripteurs.append(url)
     html = urlopen(url)
-    bs = BeautifulSoup(html.read(),'lxml')
-    descripteurs.append(str(bs.find('table').find_all('tr')[0].find('td').text))
-    descripteurs.append((bs.find('title').text).strip().replace(' | Books to Scrape - Sandbox',''))
-    descripteurs.append(str(bs.find('table').find_all('tr')[3].find('td').text))
-    descripteurs.append(str(bs.find('table').find_all('tr')[2].find('td').text))
-    descripteurs.append(str(bs.find('table').find_all('tr')[5].find('td').text))
-    descripteurs.append(bs.find('article',{'class':'product_page'}).find('p').text)
-    descripteurs.append(str(bs.find('ul',{'class':'breadcrumb'}).find_all('li')[2].find('a').text))
-    descripteurs.append(str(bs.find('table').find_all('tr')[6].find('td').text))
-    #Récupérer l'url complète de l'image
-    str_img = str(bs.find('div',{'class':'item active'}).find('img').attrs['src'])
+    bs = BeautifulSoup(html.read(), "lxml")
+    descripteurs.append(str(bs.find("table").find_all("tr")[0].find("td").text))
+    descripteurs.append(
+        (bs.find("title").text).strip().replace(" | Books to Scrape - Sandbox", "")
+    )
+    descripteurs.append(str(bs.find("table").find_all("tr")[3].find("td").text))
+    descripteurs.append(str(bs.find("table").find_all("tr")[2].find("td").text))
+    descripteurs.append(str(bs.find("table").find_all("tr")[5].find("td").text))
+    descripteurs.append(bs.find("article", {"class": "product_page"}).find("p").text)
+    descripteurs.append(
+        str(bs.find("ul", {"class": "breadcrumb"}).find_all("li")[2].find("a").text)
+    )
+    descripteurs.append(str(bs.find("table").find_all("tr")[6].find("td").text))
+    # Récupérer l'url complète de l'image
+    str_img = str(bs.find("div", {"class": "item active"}).find("img").attrs["src"])
     pos = get_substring_position(str_img, "/", 2)
-    descripteurs.append("http://books.toscrape.com"+ str_img[pos:])
-    #Télécharger image dans le répertoire courant
-    img_to_dowload = requests.get("http://books.toscrape.com"+ str_img[pos:])
-    path_img_file = os.getcwd()+"/"+str(bs.find('table').find_all('tr')[0].find('td').text)+".jpg"
+    descripteurs.append("http://books.toscrape.com" + str_img[pos:])
+    # Télécharger image dans le répertoire courant
+    img_to_dowload = requests.get("http://books.toscrape.com" + str_img[pos:])
+    path_img_file = (
+        os.getcwd()
+        + "/"
+        + str(bs.find("table").find_all("tr")[0].find("td").text)
+        + ".jpg"
+    )
     open(path_img_file, "wb").write(img_to_dowload.content)
     return descripteurs
 
-#Récupérer tous les livres appartenant à une catégorie
+
+# Récupérer tous les livres appartenant à une catégorie
 def get_category_elements(url, category_all_descripteurs):
     html = urlopen(url)
-    bs = BeautifulSoup(html.read(),'lxml')
-    list_books = bs.findAll('li',{'class','col-xs-6 col-sm-4 col-md-3 col-lg-3'})
+    bs = BeautifulSoup(html.read(), "lxml")
+    list_books = bs.findAll("li", {"class", "col-xs-6 col-sm-4 col-md-3 col-lg-3"})
     for book in list_books:
-        book_adr = str(book.find('a').attrs['href'])
-        #Récupérer l'url complète de l'image
+        book_adr = str(book.find("a").attrs["href"])
+        # Récupérer l'url complète de l'image
         pos = get_substring_position(book_adr, "/", 3)
-        book_url = "http://books.toscrape.com/catalogue/"+ book_adr[pos:]
+        book_url = "http://books.toscrape.com/catalogue/" + book_adr[pos:]
         descripteurs = get_book_elements(book_url)
         category_all_descripteurs.append(descripteurs)
-    li_next = bs.find('li',{'class','next'})
-    if(li_next is not None):
-        pos_start = get_substring_position(url, "/",6)
+    li_next = bs.find("li", {"class", "next"})
+    if li_next is not None:
+        pos_start = get_substring_position(url, "/", 6)
         pos_end = get_substring_position(url, "/", 7)
-        pos_start=pos_start+1
+        pos_start = pos_start + 1
         category = url[pos_start:pos_end]
-        next_button = bs.find('li',{'class','next'}).find('a')
-        url_text = next_button.attrs['href']
-        next_page = 'http://books.toscrape.com/catalogue/category/books/'+category+'/'+url_text
-        category_all_descripteurs = get_category_elements(next_page, category_all_descripteurs)
+        next_button = bs.find("li", {"class", "next"}).find("a")
+        url_text = next_button.attrs["href"]
+        next_page = (
+            "http://books.toscrape.com/catalogue/category/books/"
+            + category
+            + "/"
+            + url_text
+        )
+        category_all_descripteurs = get_category_elements(
+            next_page, category_all_descripteurs
+        )
     return category_all_descripteurs
 
-#Récupérer tous les livres présentés sur le site
+
+# Récupérer tous les livres présentés sur le site
 def get_all_books_elements(url, parent_dir):
     html = urlopen(url)
-    bs = BeautifulSoup(html.read(),'lxml')
+    bs = BeautifulSoup(html.read(), "lxml")
     list_category = bs.select("div > ul > li > ul > li > a")
-    all_descripteurs =[]
+    all_descripteurs = []
     for category in list_category:
-        category_descripteurs=[]
+        category_descripteurs = []
 
         # Création d'un répertoire pour regrpuper tous les fichiers générés pour la catégorie en cours
         directory = (category.text).strip()
@@ -75,28 +94,62 @@ def get_all_books_elements(url, parent_dir):
         os.mkdir(path)
         os.chdir(path)
 
-        category_url = "http://books.toscrape.com/"+category['href']
-        category_descripteurs = get_category_elements(category_url, category_descripteurs)
+        category_url = "http://books.toscrape.com/" + category["href"]
+        category_descripteurs = get_category_elements(
+            category_url, category_descripteurs
+        )
         push_category_data_to_csv(category_descripteurs, (category.text).strip())
         os.chdir(parent_dir)
 
-#Pousser les données récupérées dans un fichier CSV pour chacune des category
+
+# Pousser les données récupérées dans un fichier CSV pour chacune des category
 def push_category_data_to_csv(list_descripteurs, category):
-    path_file = os.getcwd()+"/"+category+".csv"
-    csv_file = open(path_file,'w+', newline='')
-    writer = csv.writer(csv_file, delimiter=';')
-    writer.writerow(('product_page_url', 'universal_ product_code (upc)','title','price_including_tax','price_excluding_tax','number_available','product_description','category','review_rating','image_url'))
+    path_file = os.getcwd() + "/" + category + ".csv"
+    csv_file = open(path_file, "w+", newline="")
+    writer = csv.writer(csv_file, delimiter=";")
+    writer.writerow(
+        (
+            "product_page_url",
+            "universal_ product_code (upc)",
+            "title",
+            "price_including_tax",
+            "price_excluding_tax",
+            "number_available",
+            "product_description",
+            "category",
+            "review_rating",
+            "image_url",
+        )
+    )
     for descripteurs in list_descripteurs:
-        writer.writerow((descripteurs[0], descripteurs[1], descripteurs[2], descripteurs[3], descripteurs[4], descripteurs[5], descripteurs[6], descripteurs[7], descripteurs[8], descripteurs[9]))
+        writer.writerow(
+            (
+                descripteurs[0],
+                descripteurs[1],
+                descripteurs[2],
+                descripteurs[3],
+                descripteurs[4],
+                descripteurs[5],
+                descripteurs[6],
+                descripteurs[7],
+                descripteurs[8],
+                descripteurs[9],
+            )
+        )
     csv_file.close()
 
 
 # ================== récupérer tous les livres
 now_begin = datetime.datetime.now()
-print(str(now_begin.hour)+' h', str(now_begin.minute)+' min', str(now_begin.second)+' s')
+print(
+    str(now_begin.hour) + " h",
+    str(now_begin.minute) + " min",
+    str(now_begin.second) + " s",
+)
 parent_dir = os.getcwd()
-url ='http://books.toscrape.com/'
+url = "http://books.toscrape.com/"
 get_all_books_elements(url, parent_dir)
 now_end = datetime.datetime.now()
-print(str(now_end.hour)+' h', str(now_end.minute)+' min', str(now_end.second)+' s')
-
+print(
+    str(now_end.hour) + " h", str(now_end.minute) + " min", str(now_end.second) + " s"
+)
--- one_book/get_one_book.py
+++ one_book/get_one_book.py
@@ -5,56 +5,99 @@ import csv
 import os
 import datetime
 
-#Récupérer la position d'une sous-chaîne dans une chaîne de caractères
+# Récupérer la position d'une sous-chaîne dans une chaîne de caractères
 def get_substring_position(s, str, n):
     sep = s.split(str, n)
     if len(sep) <= n:
         return -1
     return len(s) - len(sep[-1]) - len(str)
 
-#Récupérer les éléments de description d'un livre
+
+# Récupérer les éléments de description d'un livre
 def get_book_elements(url):
-    descripteurs =[]
+    descripteurs = []
     descripteurs.append(url)
     html = urlopen(url)
-    bs = BeautifulSoup(html.read(),'lxml')
-    descripteurs.append(str(bs.find('table').find_all('tr')[0].find('td').text))
-    descripteurs.append((bs.find('title').text).strip().replace(' | Books to Scrape - Sandbox',''))
-    descripteurs.append(str(bs.find('table').find_all('tr')[3].find('td').text))
-    descripteurs.append(str(bs.find('table').find_all('tr')[2].find('td').text))
-    descripteurs.append(str(bs.find('table').find_all('tr')[5].find('td').text))
-    descripteurs.append(bs.find('article',{'class':'product_page'}).find('p').text)
-    descripteurs.append(str(bs.find('ul',{'class':'breadcrumb'}).find_all('li')[2].find('a').text))
-    descripteurs.append(str(bs.find('table').find_all('tr')[6].find('td').text))
-    #Récupérer l'url complète de l'image
-    str_img = str(bs.find('div',{'class':'item active'}).find('img').attrs['src'])
+    bs = BeautifulSoup(html.read(), "lxml")
+    descripteurs.append(str(bs.find("table").find_all("tr")[0].find("td").text))
+    descripteurs.append(
+        (bs.find("title").text).strip().replace(" | Books to Scrape - Sandbox", "")
+    )
+    descripteurs.append(str(bs.find("table").find_all("tr")[3].find("td").text))
+    descripteurs.append(str(bs.find("table").find_all("tr")[2].find("td").text))
+    descripteurs.append(str(bs.find("table").find_all("tr")[5].find("td").text))
+    descripteurs.append(bs.find("article", {"class": "product_page"}).find("p").text)
+    descripteurs.append(
+        str(bs.find("ul", {"class": "breadcrumb"}).find_all("li")[2].find("a").text)
+    )
+    descripteurs.append(str(bs.find("table").find_all("tr")[6].find("td").text))
+    # Récupérer l'url complète de l'image
+    str_img = str(bs.find("div", {"class": "item active"}).find("img").attrs["src"])
     pos = get_substring_position(str_img, "/", 2)
-    descripteurs.append("http://books.toscrape.com"+ str_img[pos:])
-    #Télécharger image dans le répertoire courant
-    img_to_dowload = requests.get("http://books.toscrape.com"+ str_img[pos:])
-    path_img_file = os.getcwd()+"/"+str(bs.find('table').find_all('tr')[0].find('td').text)+".jpg"
+    descripteurs.append("http://books.toscrape.com" + str_img[pos:])
+    # Télécharger image dans le répertoire courant
+    img_to_dowload = requests.get("http://books.toscrape.com" + str_img[pos:])
+    path_img_file = (
+        os.getcwd()
+        + "/"
+        + str(bs.find("table").find_all("tr")[0].find("td").text)
+        + ".jpg"
+    )
     open(path_img_file, "wb").write(img_to_dowload.content)
     return descripteurs
 
-#Pousser les données récupérées dans un fichier CSV
+
+# Pousser les données récupérées dans un fichier CSV
 def push_data_to_csv(list_descripteurs):
-    path_file = os.getcwd()+'\csv_file.csv'
-    csv_file = open(path_file,'w+', newline='')
-    writer = csv.writer(csv_file, delimiter=';')
-    writer.writerow(('product_page_url', 'universal_ product_code (upc)','title','price_including_tax','price_excluding_tax','number_available','product_description','category','review_rating','image_url'))
+    path_file = os.getcwd() + "\csv_file.csv"
+    csv_file = open(path_file, "w+", newline="")
+    writer = csv.writer(csv_file, delimiter=";")
+    writer.writerow(
+        (
+            "product_page_url",
+            "universal_ product_code (upc)",
+            "title",
+            "price_including_tax",
+            "price_excluding_tax",
+            "number_available",
+            "product_description",
+            "category",
+            "review_rating",
+            "image_url",
+        )
+    )
     for descripteurs in list_descripteurs:
-        writer.writerow((descripteurs[0], descripteurs[1], descripteurs[2], descripteurs[3], descripteurs[4], descripteurs[5], descripteurs[6], descripteurs[7], descripteurs[8], descripteurs[9]))
+        writer.writerow(
+            (
+                descripteurs[0],
+                descripteurs[1],
+                descripteurs[2],
+                descripteurs[3],
+                descripteurs[4],
+                descripteurs[5],
+                descripteurs[6],
+                descripteurs[7],
+                descripteurs[8],
+                descripteurs[9],
+            )
+        )
     csv_file.close()
+
 
 # ================== récupérer un seul livre
 now_begin = datetime.datetime.now()
-print(str(now_begin.hour)+' h', str(now_begin.minute)+' min', str(now_begin.second)+' s')
-path_file = os.getcwd()+'/csv_file1.csv'
-url='http://books.toscrape.com/catalogue/choosing-our-religion-the-spiritual-lives-of-americas-nones_14/index.html'
-list_descripteurs=[]
+print(
+    str(now_begin.hour) + " h",
+    str(now_begin.minute) + " min",
+    str(now_begin.second) + " s",
+)
+path_file = os.getcwd() + "/csv_file1.csv"
+url = "http://books.toscrape.com/catalogue/choosing-our-religion-the-spiritual-lives-of-americas-nones_14/index.html"
+list_descripteurs = []
 descripteurs = get_book_elements(url)
 list_descripteurs.append(descripteurs)
 push_data_to_csv(list_descripteurs)
 now_end = datetime.datetime.now()
-print(str(now_end.hour)+' h', str(now_end.minute)+' min', str(now_end.second)+' s')
-
+print(
+    str(now_end.hour) + " h", str(now_end.minute) + " min", str(now_end.second) + " s"
+)
